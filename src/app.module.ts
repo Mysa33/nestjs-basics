9bf76d48c39a6a34cbea314e99af65b8efb47830
Mysa33 <tpdrn.france@gmail.com>
2019-05-09 16:29:39 +0200

    bug fix : db connection OK

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,7 @@ import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
-    BlogModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      username: 'root',
-      password: '',
-      database: 'miage',
-      entities: ["src/**/**.entity.{ts,js}"],
-      synchronize: true,
-    })
+    BlogModule
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {ArticleEntity} from "./blog/entities/article.entity";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
+
+createConnection({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "",
+  database: "miage",
+  entities: [
+      ArticleEntity
+  ],
+  synchronize: true,
+  logging: false
+}).then(connection => {
+  console.log("DB connected ! ", connection);
+}).catch(error => console.log(error));
